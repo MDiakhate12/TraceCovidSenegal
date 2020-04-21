13a930e6980275ffe3535234e2d685ae669e9228
BathieSG7 <34387761+BathieSG7@users.noreply.github.com>
2020-04-21 18:12:08 +0000

    firebase configured for ios

--- ios/TraceCovidSenegal/AppDelegate.m
+++ ios/TraceCovidSenegal/AppDelegate.m
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -27,6 +27,11 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+}
+  
 #if DEBUG
   InitializeFlipper(application);
 #endif
@@ -56,3 +61,7 @@ static void InitializeFlipper(UIApplication *application) {
 }
 
 @end
+
+
+
+
